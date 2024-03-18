f39c78949e7c5570ffd5680366bab5251916922d
josecdomber <josecdomber@gmail.com>
2024-03-18 19:12:03 +0100

    Save session 1 of 4 coding xrpl

--- apps/starter/src/index.ts
+++ apps/starter/src/index.ts
@@ -1,5 +1,21 @@
+import { sendPayment } from "./transactions"
+import { WALLET_1, WALLET_2 } from "./wallet"
+import { getClient } from "./xrpl-client"
+
+const client = getClient()
+
 const main = async () => {
-  console.log("It works!")
+  await client.connect()
+
+  await sendPayment(
+    {
+      Destination: WALLET_2.address,
+      Amount: "10000000",
+    },
+    { wallet: WALLET_1 }
+  )
+
+  await client.disconnect()
 }
 
 main()
